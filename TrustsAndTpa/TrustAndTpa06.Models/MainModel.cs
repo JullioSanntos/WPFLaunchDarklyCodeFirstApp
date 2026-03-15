8b28d12e59b2fa0bbb5bef6e4dc730ec1cffa381
JullioSanntos <JulioSantos@Hotmail.com>
2026-03-15 09:36:18 -0500

    MainViewModel included

---
+++ TrustsAndTpa/TrustAndTpa06.Models/MainModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrustsAndTpa.TrustAndTpa12.Infrastructure;
+
+namespace TrustsAndTpa.TrustAndTpa06.Models; 
+public class MainModel : SetPropertyBase {
+
+    #region properties
+    #region OutgoingAccount
+    public string? OutgoingAccount {
+        get;
+        set => SetProperty(ref field, value);
+    }
+    #endregion OutgoingAccount
+
+    #region InboundAccount
+    public string? InboundAccount {
+        get;
+        set => SetProperty(ref field, value);
+    }
+    #endregion InboundAccount
+    #endregion properties
+
+    #region methods
+    public async Task<string?> GetAccountAsync(string accountNumber) { await Task.Delay(0); return null; }
+    public async Task<bool> AddAccount(string accountNumber) { await Task.Delay(0); return false; }
+    public async Task<bool> RemoveAccount(string accountNumber) { await Task.Delay(0); return false; }
+    public async Task<bool> ValidateAccountAsync(string accountNumber) { await Task.Delay(0); return false; }
+    public async Task<bool> UpdateAccounTask(string accountNumber) { await Task.Delay(0); return false; }
+    #endregion methods
+
+}
